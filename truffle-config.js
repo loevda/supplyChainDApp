9aa0ac2c4600b07dbf8beca899d14017b331c0e8
david2099 <loevenbruck.david@gmail.com>
2019-02-23 20:49:30 +0100

    chore: fix out of gas issue during compilation

--- truffle-config.js
+++ truffle-config.js
@@ -16,23 +16,23 @@ module.exports = {
             port: 9545,
             network_id: '*', // Match any network id
         },
-        /*ganache: {
+        ganache: {
             host: '127.0.0.1',
             port: 7545,
             network_id: '*', // Match any network id
             gas: 3000000000,
-        },*/
+        },
         rinkeby: {
             provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
             network_id: '4',
             gas: 4500000,
             gasPrice: 10000000000,
         },
-        solc: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
         }
     }
 }
